43f9c07637f528e6079b1fb60159fd1b2713a625
”aniakin” <ania.grajek@gmail.com>
2023-12-07 23:44:25 +0200

    fixing the links

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function changeImage(imageName) {
-    const mainImage = document.getElementById('main-image');
+document.addEventListener("DOMContentLoaded", function() {
+    // Tässä vaiheessa DOM on valmis
+    // Voit laittaa täällä muita koodisi
   
-    // Tarkista, että elementti on olemassa ennen kuin yrität muuttaa sen src-ominaisuutta
-    if (mainImage) {
-      mainImage.src = `images/${imageName}`;
-    } else {
-      console.error('Element with ID "main-image" not found.');
+    function changeImage(imageName) {
+      const mainImage = document.getElementById('main-image');
+    
+      if (mainImage) {
+        mainImage.src = `images/${imageName}`;
+      } else {
+        console.error('Element with ID "main-image" not found.');
+      }
     }
-  }
-  
-  
+  });
