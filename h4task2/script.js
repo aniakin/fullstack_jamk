eb76e01e4a4037094e485a7255138752e41c1353
”aniakin” <ania.grajek@gmail.com>
2023-12-09 13:55:40 +0200

    fix

--- h4task2/script.js
+++ h4task2/script.js
@@ -1,5 +1,3 @@
-let housediv;
-
 async function getHouses() {
     const response = await fetch('talotiedot.json');
     const data = await response.json();
@@ -10,19 +8,12 @@ async function renderHouses() {
     let houses = await getHouses();
     console.log(houses);
 
+    let housediv = document.getElementById("houses");
     housediv.innerHTML = "";
 
-    let checkbox1 = createCheckbox('filterSize', 'Näytä alle 200m2');
-    let checkbox2 = createCheckbox('filterPrice', 'Näytä alle 1 000 000 €');
-
-    housediv.appendChild(checkbox1.label);
-    housediv.appendChild(checkbox1.checkbox);
-    housediv.appendChild(checkbox2.label);
-    housediv.appendChild(checkbox2.checkbox);
-
     houses.forEach(house => {
-        const showBySize = !checkbox1.checkbox.checked || (checkbox1.checkbox.checked && house.size < 200);
-        const showByPrice = !checkbox2.checkbox.checked || (checkbox2.checkbox.checked && house.price < 1000000);
+        const showBySize = !checkbox1.checked || (checkbox1.checked && house.size < 200);
+        const showByPrice = !checkbox2.checked || (checkbox2.checked && house.price < 1000000);
 
         if (showBySize && showByPrice) {
             let housecontainer = document.createElement('div');
@@ -62,21 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
     housediv = document.getElementById("houses");
     renderHouses();
 
-    housediv.addEventListener('change', (event) => {
-        if (event.target.type === 'checkbox') {
-            updateView();
-        }
+    housediv.addEventListener('change', () => {
+        updateView();
     });
 });
-
-function createCheckbox(id, labelText) {
-    let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = id;
-
-    let label = document.createElement('label');
-    label.htmlFor = id;
-    label.innerHTML = labelText;
-
-    return { checkbox, label };
-}
