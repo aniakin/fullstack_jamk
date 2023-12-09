d93da2f401ea131e5c6c6356228c43a71c1bb455
”aniakin” <ania.grajek@gmail.com>
2023-12-09 18:47:08 +0200

    filtering

--- h4task2/script.js
+++ h4task2/script.js
@@ -44,4 +44,52 @@ function createCheckbox(id, label) {
     checkbox.checked = checkboxState[id]; // Set the initial state
 
     let labelElement = document.createElement('label');
-    lab
+    labelElement.for = id;
+    labelElement.innerHTML = label;
+    labelElement.appendChild(checkbox);
+
+    return labelElement;
+}
+
+function createHouseContainer(house) {
+    let housecontainer = document.createElement('div');
+    housecontainer.className = 'houseContainer';
+    housecontainer.dataset.size = house.size;
+    housecontainer.dataset.price = house.price;
+
+    let image = document.createElement('img');
+    image.src = 'images/' + house.image;
+    image.className = 'houseImage';
+
+    let header = document.createElement('p');
+    header.className = 'header';
+    header.innerHTML = house.address;
+
+    let sizeText = document.createElement('p');
+    sizeText.className = 'text';
+    sizeText.innerHTML = 'Pinta-ala: ' + house.size + ' m²';
+
+    let priceText = document.createElement('p');
+    priceText.className = 'text';
+    priceText.innerHTML = 'Hinta: ' + new Intl.NumberFormat('fi-FI').format(house.price) + ' €';
+
+    housecontainer.appendChild(image);
+    housecontainer.appendChild(header);
+    housecontainer.appendChild(sizeText);
+    housecontainer.appendChild(priceText);
+
+    return housecontainer;
+}
+
+function updateView() {
+    let houses = document.querySelectorAll('.houseContainer');
+
+    houses.forEach(house => {
+        let showBySize = !checkboxState.filterSize || (checkboxState.filterSize && house.dataset.size < 200);
+        let showByPrice = !checkboxState.filterPrice || (checkboxState.filterPrice && house.dataset.price < 1000000);
+
+        house.style.display = showBySize && showByPrice ? 'block' : 'none';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', renderHouses);
